7e0c2d097f0677c043f98ebb8523eaf21756f8c8
Long <vtran3331@gmail.com>
2026-08-08 22:58:42 +0700

    New

---
+++ Medium/48_Rotate Image.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        // Bước 1: Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Bước 2: Reverse từng hàng
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+};
---
+++ Medium/50_Pow(x, n).cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        x=pow(x,n);
+        return x;
+        
+    }
+};
